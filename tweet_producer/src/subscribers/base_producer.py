60f0de4b35cb8154eed5cf5f1cb9b68eda649997
Fhuad Balogun <fhuadbalogun@gmail.com>
2022-03-14 10:26:01 +0100

    refactor

--- tweet_producer/src/subscribers/base_producer.py
+++ tweet_producer/src/subscribers/base_producer.py
@@ -4,10 +4,6 @@ sys.path.insert(0, BASE)
 from kafka import KafkaProducer
 from utils.const import KAFKA_SERVER
 
-# producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
 class Producer(KafkaProducer):
     pass
-
-# producer.send(topic, data)
-# producer.flush()
